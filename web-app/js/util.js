404fa39dd2e21e942bb2c6926a7d4d656cba55e4
Michael Trans <31951247+mich282q@users.noreply.github.com>
2020-11-19 12:00:31 +0100

    g

---
+++ web-app/js/util.js
@@ -0,0 +1,4 @@
+const DOMAIN = "http://localhost/flybook/";
+var API_KEY = 'Flight%20booker%201.2%20plus%20edition';
+var URL = DOMAIN + 'api/API.php?API_KEY='+API_KEY;
+var URL_STATUS = DOMAIN + 'cgi-bin/status.cgi?';
